ba0f841a643afd307423257e92102add710e999f
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-07-27 17:42:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const cluck = farmAnimalsArray[4]
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them Bessie, Dolly, Babe, and Little.
 const Bessie = moo
 const Dolly = baa
-const babe = oink
+const Babe = oink
 const little = cluck
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
