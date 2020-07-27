7e5e58821c79f69604eb013eefd46167bb1bf8fc
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-07-27 17:58:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const indigo = colors[5]
 const violet = colors[6]
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+const r = red
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
