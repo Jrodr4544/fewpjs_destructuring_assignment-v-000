72b177ef8010859f245097ceec05395ad682105d
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-07-27 18:05:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ const indg = indigo
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
+const { name } = muppet.name
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
